96da0aedd8b165a96a2f1dc3669961939a395a3a
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-07-03 19:33:23 -0300

    Búsqueda de una palabra en el pdf importado

--- machine_readable_pdfs.py
+++ machine_readable_pdfs.py
@@ -1,24 +1,46 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTextEdit
+from PyPDF2 import PdfReader
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Importe de PDF")
-        self.setGeometry(100, 100, 400, 200)
+        self.setGeometry(100, 100, 400, 300)
 
         self.button = QPushButton("Importar PDF", self)
         self.button.setGeometry(150, 80, 100, 30)
         self.button.clicked.connect(self.import_pdf)
 
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(50, 120, 300, 150)
+
     def import_pdf(self):
         file_dialog = QFileDialog()
-        file_dialog.setFileMode(QFileDialog.ExistingFiles)
+        file_dialog.setFileMode(QFileDialog.ExistingFile)
         file_dialog.setNameFilter("Archivos PDF (*.pdf)")
         if file_dialog.exec_():
-            file_names = file_dialog.selectedFiles()
-            for file_name in file_names:
-                print("Nombre del pdf:", file_name)
+            file_name = file_dialog.selectedFiles()[0]
+
+            texto_encontrado = self.buscar_dato_pdf(file_name, "Palabra")
+
+            if texto_encontrado:
+                self.text_edit.setPlainText(texto_encontrado)
+            else:
+                self.text_edit.setPlainText("La palabra ingresada no se encontró en el PDF.")
+
+    def buscar_dato_pdf(self, archivo_pdf, dato_buscado):
+        texto_encontrado = ""
+        with open(archivo_pdf, 'rb') as archivo:
+            lector_pdf = PdfReader(archivo)
+            num_paginas = len(lector_pdf.pages)
+
+            for pagina in range(num_paginas):
+                contenido_pagina = lector_pdf.pages[pagina].extract_text()
+                if dato_buscado in contenido_pagina:
+                    texto_encontrado += contenido_pagina
+
+        return texto_encontrado
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
